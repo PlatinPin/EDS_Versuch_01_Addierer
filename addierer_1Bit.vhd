a0ffcb37cacbdf0912a5eda048764a5c3cde6821
PlatinPin <daniel.lipaj.2001@gmail.com>
2023-09-26 11:34:42 +0200

    Datei addierer-1Bit hinzugefügt
    1Bit-Volladdierer bestehend aus Digitalen Grundelementen.

---
+++ addierer_1Bit.vhd
@@ -0,0 +1,18 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity addierer_1Bit is
+    Port (
+        c_in, a,b : in std_logic;
+        c_out, y: out std_logic 
+    );
+end addierer_1Bit;
+
+architecture Behavioral of addierer_1Bit is
+begin
+    y <= (a xor b) xor c_in;
+    c_out <= (b and c_in) or (a and c_in) or (a and b);
+
+
+end Behavioral;
